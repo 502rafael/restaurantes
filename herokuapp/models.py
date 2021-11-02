b2e8872e3765d0498a745a7d5eed257a221b246c
VICTOR RAFAEL OLMINO SUMPANGO <volminos@miumg.edu.gt>
2021-11-01 21:02:22 -0600

    Mi branchddddd

--- herokuapp/models.py
+++ herokuapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Alimentos(models.Model):
+    nombre = models.CharField(max_length = 100)
+    descripcion = models.CharField(max_length = 100)
+    precio = models.CharField(max_length = 200)
